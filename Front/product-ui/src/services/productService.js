c1b252c950786107274be4809552943d8b3feab4
farhad <f.karimi8216@gmail.com>
2025-11-04 14:46:36 +0330

    Delete Front/product-ui directory

--- Front/product-ui/src/services/productService.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const API_URL = "http://localhost:8080/api/products";
-
-export const getAllProducts = async () => {
-    const response = await axios.get(API_URL);
-    return response.data;
-};
